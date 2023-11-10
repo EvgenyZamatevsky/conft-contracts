129a932f780af1f7d84974ed67b10be0b0a6632f
nilstruct <nilstruct@gmail.com>
2023-11-10 11:30:52 +0600

    Use gasreporter for tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,17 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    excludeContracts: ['/test']
+  }
 };
